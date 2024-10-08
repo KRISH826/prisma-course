13a099fc29897e69b0e1596a1691cc64a719dd12
KRISH826 <krishnendupanj@gmail.com>
2024-10-09 02:39:37 +0530

    complete prisma crash course

--- src/routes/PostRouter.js
+++ src/routes/PostRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts, searchPost, updatePost } from "../controllers/post.controller.js";
 
 
 const router = Router();
 router.get("/", getPosts);
 router.post("/", createPost);
+router.get("/search", searchPost)
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
+
 
 export default router;
